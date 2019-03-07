b79313f0260dda88beed658ce314f807a16cbd27
Grant Chan <grant.r.chan@gmail.com>
2019-03-06 23:35:09 -0600

    fixed navigation for mobile devices

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,18 @@ var path = require('path');
 module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
-        path: "./app/temp/scripts",
         // generate absolute path
         path: path.resolve(__dirname, "./app/temp/scripts"),
         filename: "App.js"
+    },
+    module:{
+        loaders:[{
+            loader: 'babel-loader',
+            query:{
+                presets:['es2015']
+            },
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
-}
+};
